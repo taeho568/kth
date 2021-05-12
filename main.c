12027251ede135877fe2159fa3712aeb3a8395ec
taeho568 <xogh1633@naver.com>
2021-05-12 16:34:55 +0900

    Modify main.c in tsetbrach

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-#include "myheader.h"
+#include "myheader.h
+"
 int main(void) {
+    char name[17] = {0,};
+    printf("Name: ");
+    scanf("%s", name);
     printHello();
     printBye();
     return 0;
